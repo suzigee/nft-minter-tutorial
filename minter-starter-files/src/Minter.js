845bc90dc92661893e2b3a76cf00eca62e1e18bd
suzigee <98690672+suzigee@users.noreply.github.com>
2022-04-15 15:32:37 +0100

    minter and inteact updates

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-import { connectWallet } from "./Utils/interact.js";
+import {
+  connectWallet,
+  getCurrentWalletConnected //import here
+} from "./utils/interact.js";
 
 const Minter = (props) => {
 
@@ -10,9 +13,11 @@ const Minter = (props) => {
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
  
-  useEffect(async () => { //TODO: implement
-    
-  }, []);
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status); 
+}, []);
 
   const connectWalletPressed = async () => {
     const walletResponse = await connectWallet();
